ca7da7622eccdef1326f6533ee0dfdd917730582
desarrollador00 <brayanrosalescontreras@gmail.com>
2025-02-26 22:35:23 -0500

    database

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import mysql.connector
+from datetime import datetime
+
+db = mysql.connector.connect(
+	host=os.getenv("MYSQL_HOST"),
+	user=os.getenv("MYSQL_USER"),
+	password=os.getenv("MYSQL_PASSWORD"),
+	database=os.getenv("MYSQL_DATABASE"),
+	port=int(os.getenv("MYSQL_PORT", 3306))
+)
 
 class sensor(BaseModel):
-	lon: int 
-	lat: int 
-	pos: int 
-	ult: int
-
+	lon: float
+	lat: float
+	pos: str
+	ult: float
 app = FastAPI()
 
 @app.post("/data")
@@ -16,7 +25,22 @@ async def insert_data(data_sensor: sensor):
 	"""
 	Inserta los datos a la base de datos
 	"""
-	return sensor
+	cursor = db.cursor()
+	query = """
+		INSERT INTO datos_baston (latitud, longitud, posicion, fecha, alerta)
+		VALUES (%s, %s, %s, %s, %s)
+    """
+	values = (data.lat, data.lon, data.pos, datetime.now(), data.ult)
+	try:
+		with db.cursor() as cursor:
+			cursor.execute(query, values)
+			db.commit()
+		return {"message": "Datos almacenados correctamente"}
+	except Exception as e:
+		db.rollback()
+		return {"error": str(e)}
+	finally:
+		cursor.close()
 	
 if __name__ == "__main__":
 	port = int(os.getenv("PORT", 8080))  # 🚀 Usa el puerto que asigna Railway
