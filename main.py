9ea96dc54e42ea1286f5e366ac0a80929082bb66
desarrollador00 <brayanrosalescontreras@gmail.com>
2025-02-26 21:44:55 -0500

    insert_data

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class sensor(BaseModel):
+	lon: int 
+	lat: int 
+	pos: int 
+	ult: int
+
+app = FastAPI()
+
+@router.post("/data")
+async def insert_data(data_sensor: sensor):
+	"""
+	Inserta los datos a la base de datos
+	"""
+	return sensor
+	
