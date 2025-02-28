658b0909d7dd1876a7ad8b071ed032551e811e7c
desarrollador00 <brayanrosalescontreras@gmail.com>
2025-02-27 21:37:17 -0500

    alerta_baston

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def insert_data(data_sensor: sensor):
 		INSERT INTO datos_baston (latitud, longitud, posicion, fecha, alerta)
 		VALUES (%s, %s, %s, %s, %s)
     """
-	values = (data_sensor.lon, data_sensor.lat, data_sensor.pos, datetime.now(), data_sensor.ult)
+	values = (data_sensor.lon, data_sensor.lat, data_sensor.pos, datetime.now(), data_sensor.alm)
 	try:
 		with db.cursor() as cursor:
 			cursor.execute(query, values)
