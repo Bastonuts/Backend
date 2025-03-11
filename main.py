cf072ec0476e62184b2536d5821536fb82e22e51
desarrollador00 <brayanrosalescontreras@gmail.com>
2025-03-11 08:49:37 -0500

    fixed

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def insert_data(data_sensor: sensor):
 	query = """
 		INSERT INTO datos_baston (latitud, longitud, posicion, fecha, alerta)
 		VALUES (%s, %s, %s, %s, %s)
-    """
+	"""
 	values = (data_sensor.lon, data_sensor.lat, data_sensor.pos, datetime.now(), data_sensor.alarm)
 	try:
 		with db.cursor() as cursor:
@@ -44,5 +44,5 @@ async def insert_data(data_sensor: sensor):
 	#return sensor
 	
 if __name__ == "__main__":
-	port = int(os.getenv("PORT", 3306))  # 🚀 Usa el puerto que asigna Railway
+	port = int(os.getenv("PORT", 8080))  # 🚀 Usa el puerto que asigna Railway
 	uvicorn.run(app, host="0.0.0.0", port=port)
